608533f3b23d79157d47c9d724a5bc8d3510fe92
youngmoguler <sudhanshushukla902@gmail.com>
2023-09-19 09:40:50 +0530

    Added and Signed Homebrew Tap for emmet-language-server#3

--- Formula/emmet-ls.rb
+++ Formula/emmet-ls.rb
@@ -4,8 +4,10 @@ class EmmetLs < Formula
   url "https://github.com/youngmoguler/emmet-language-server/archive/refs/tags/v2.0.0.tar.gz"
   sha256 "58a77cf0a7a5459876d797f401a7ffecb35a1fb9aaaa794e09c00d9abf9b7fd9"
 
+    depends_on "node" => :build
+
   def install
-    system "npm", "install", *std_npm_args
+    system "npm", "install"
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
